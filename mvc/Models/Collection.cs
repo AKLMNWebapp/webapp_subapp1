adf114bc3d05305df7c5ae043863d9aafc266368
Nadin Hessen <s364762@oslomet.no>
2024-10-07 21:42:30 +0200

    Added LazyLoading

--- mvc/Models/Collection.cs
+++ mvc/Models/Collection.cs
@@ -4,8 +4,8 @@ namespace mvc.Models;
 public class Collection {
     public int CollectionId {get; set;}
     public int UserId {get; set;}
-    public User User { get; set; } = default!; //navigation property
+    public virtual User User { get; set; } = default!; //navigation property
     public int ProductId {get; set;}
-    public Product Product {get; set; } = default!; //navigation property
+    public virtual Product Product {get; set; } = default!; //navigation property
     public DateTime? CreatedAt {get; set;}
 }
