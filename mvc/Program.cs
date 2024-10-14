44717d94e4ac8ebbcbd9be9690fe4fe28dc6d6d8
angeclara <141249648+angeclara@users.noreply.github.com>
2024-10-15 00:07:19 +0200

    filters out logging of inromation level + executed DbCommand

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using mvc.DAL;
 using mvc.Models;
 using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,10 @@ builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
 var loggerConfiguration = new LoggerConfiguration()
     .MinimumLevel.Information() // Levels: Trace< Information < Warning < Error < Fatal
     .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd:HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) &&
+                            e.Level == LogEventLevel.Information &&
+                            e.MessageTemplate.Text.Contains("Excecuted DbCommand"));
 
 var logger = loggerConfiguration.CreateLogger();
 builder.Logging.AddSerilog(logger);
