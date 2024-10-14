63948e311b30c955903bbe14a39d02510f443cc7
angeclara <141249648+angeclara@users.noreply.github.com>
2024-10-14 22:50:09 +0200

    Updated Create/Updtae product

--- mvc/Controllers/ProductController.cs
+++ mvc/Controllers/ProductController.cs
@@ -34,12 +34,12 @@ public class ProductController : Controller
         return View(product);
     }
 
-    /*
+    
     // This Get request populates the Allergy section with already existing allergies in the database
     [HttpGet]
     public async Task<IActionResult> CreateProduct() 
     {
-        var allergies = await _context.Allergies.ToListAsync(); // gets list of all available allergies
+        var allergies = await _productRepository.GetAll(); // gets list of all available allergies
 
         // Our viewModel here is used to list all allergies in our select menu on the view
         var createProductViewModel = new CreateProductViewModel
@@ -72,8 +72,8 @@ public class ProductController : Controller
             product.UserId = int.Parse(userID); // parses id to string to use in if statement
             if (!string.IsNullOrEmpty(model.NewAllergyName)) {
                 var newAllergy = new Allergy {Name = model.NewAllergyName}; // Creates a new allergy
-                _context.Allergies.Add(newAllergy);
-                await _context.SaveChangesAsync(); // Saves new allergy to database
+                _productRepository.Allergies.Add(newAllergy);
+                await _productRepository.SaveChangesAsync(); // Saves new allergy to database
                 model.SelectedAllergyCodes.Add(newAllergy.AllergyCode); // adds the new allergies codes to viewModel
             }
 
@@ -87,13 +87,52 @@ public class ProductController : Controller
                 });
             }
 
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync(); // updated the db with new product
+            _productRepository.Products.Add(product);
+            await _productRepository.SaveChangesAsync(); // updated the db with new product
             return RedirectToAction("Index"); // redirects to Index view.
         }
         return View(product);
     }
-    */
+
+    [HttpPost]
+    public async Task<IActionResult> Update(CreateProductViewModel model) 
+    {
+        var product = model.Product;
+        if (ModelState.IsValid)
+        {
+
+            // This code ensures that the product can only be made by users, that are logged in
+            var userID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userID))
+            {
+                return Forbid();
+            }
+
+            // This code handles user with self-made allergies that are not already pre existing in out database
+            product.UserId = int.Parse(userID); // parses id to string to use in if statement
+            if (!string.IsNullOrEmpty(model.NewAllergyName)) {
+                var newAllergy = new Allergy {Name = model.NewAllergyName}; // Creates a new allergy
+                _productRepository.Allergies.Add(newAllergy);
+                await _productRepository.SaveChangesAsync(); // Saves new allergy to database
+                model.SelectedAllergyCodes.Add(newAllergy.AllergyCode); // adds the new allergies codes to viewModel
+            }
+
+            // This loop iterates through all allergy codes that have been selected from the allergy menu
+            // The selected allergies will be saved as an AllergyProduct
+            foreach (var allergyCode in model.SelectedAllergyCodes)
+            {
+                product.AllergyProducts.Add(new AllergyProduct {
+                    AllergyCode = allergyCode,
+                    Product = product
+                });
+            }
+
+            _productRepository.Products.Update(product);
+            await _productRepository.SaveChangesAsync(); // updated the db with new product
+            return RedirectToAction("Index"); // redirects to Index view.
+        }
+        return View(product);
+    }
     public async Task<IActionResult> Delete(int id)
     {
         var product = await _productRepository.GetProductById(id);
