61c2e78a9f19e9eea4f647cb669066bd4fe1e4a1
angeclara <141249648+angeclara@users.noreply.github.com>
2024-10-15 12:57:48 +0200

    restoringggg

--- mvc/Program.cs
+++ mvc/Program.cs
@@ -24,4 +24,29 @@ loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceCont
                             e.Level == LogEventLevel.Information &&
                             e.MessageTemplate.Text.Contains("Excecuted DbCommand"));
 
-var logger = loggerConfiguration.C
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseHsts();
+}
+DBInit.Seed(app);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
