e9f0cf26242a1b7450aa3114ab4e02984f7aa507
angeclara <141249648+angeclara@users.noreply.github.com>
2024-11-12 21:59:08 +0100

    starting with review solution

--- mvc/DAL/Models/Review.cs
+++ mvc/DAL/Models/Review.cs
@@ -10,17 +10,14 @@ public class Review
     public int ProductId {get; set;}
     public virtual Product Product {get; set;} = default!; //navigation property
 
-   [Range(0, double.MaxValue, ErrorMessage = "Rating must be a greater than 0.")]
-    public decimal Rating {get; set;}
-
    [StringLength(200)]
     public string? Comment {get; set;}
-    public DateTime? CreatedAt {get; set;}
+    public DateTime? CreatedAt {get; set;} = DateTime.Now;
 
     [StringLength(200)]
     public string? Response {get; set;}
 
     public string? ResponseUserID {get; set;}
 
-    public virtual ApplicationUser? ResponseUser {get; set;} = default!; // navigation property
+    public virtual ApplicationUser? ResponseUser {get; set;} // navigation property
 }
