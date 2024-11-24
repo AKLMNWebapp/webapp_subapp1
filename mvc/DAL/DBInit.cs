ba4ac88082bfd60471f4f68e89bc65d53d9577b6
Nadin Hessen <s364762@oslomet.no>
2024-11-24 22:19:17 +0100

    more beauty fix

--- mvc/DAL/DBInit.cs
+++ mvc/DAL/DBInit.cs
@@ -129,11 +129,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Eggs",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 155,
+                     Fat = 11,
+                     Carbohydrates = 1.1,
+                     Protein = 13,
+                     Description = "Eggs are a versatile and nutritious food, perfect for breakfast, baking, or cooking."+ 
+                                    "Rich in protein, vitamins, and minerals, they’re an excellent choice for a balanced diet."+ 
+                                    "Whether boiled, scrambled, fried, or baked, eggs are easy to prepare and a staple in many dishes.",
                      ImageUrl = "/images/eggs.jpg",
                      UserId = businessUser.Id,
                      CategoryId = 1,
@@ -150,11 +152,12 @@ public static class DBInit
                  new Product
                  {
                      Name = "Banana",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 88,
+                     Fat = 0.3,
+                     Carbohydrates = 23,
+                     Protein = 1.1,
+                     Description = "Perfect for a quick snck or addition to meals. Bananas are richin ptassium, fiber, and bitamins,"+ 
+                                    "making them a healthy chocie for energy and digestion.",
                      ImageUrl = "/images/bananas.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 4,
@@ -162,11 +165,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Asparagues",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 20,
+                     Fat = 0.1,
+                     Carbohydrates = 3.9,
+                     Protein = 2.2,
+                     Description = "A crisp and tender vegetable with a fresh, earthy flavor. Known for its long, green stalks,"+ 
+                                    "it’s a healthy choice rich in vitamins, fiber, and antioxidants."+ 
+                                    "Perfect for roasting, grilling, or steaming, asparagus makes a delicious side dish or a nutritious addition to salads and pastas.",
                      ImageUrl = "/images/asparagus.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 4,
@@ -174,11 +179,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Cheese",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 402,
+                     Fat = 33,
+                     Carbohydrates = 1.3,
+                     Protein = 25,
+                     Description = "A rich and flavorful dairy product enjoyed in countless dishes around the world."+ 
+                                    "From creamy and mild to sharp and aged, cheese comes in a variety of types to suit any taste."+
+                                    "Packed with calcium and protein, it’s perfect for snacks, sandwiches, pastas, or simply on its own",
                      ImageUrl = "/images/cheese.jpg",
                      UserId = businessUser.Id,
                      CategoryId = 1,
@@ -195,11 +202,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Vegan Cheese",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 277,
+                     Fat = 20.3,
+                     Carbohydrates = 24,
+                     Protein = 10,
+                     Description = "A dairy-free alternative made from plant-based ingredients like nuts, soy, or coconut."+ 
+                                    "With its creamy texture and rich flavor, it’s perfect for those following a vegan diet or looking for a lactose-free option."+ 
+                                    "Ideal for sandwiches, pizzas, or snacks, vegan cheese offers a tasty and versatile way to enjoy cheese without animal products.",
                      ImageUrl = "/images/vegan_cheese.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 2,
@@ -207,23 +216,27 @@ public static class DBInit
                  new Product
                  {
                      Name = "Meat",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Energy = 143,
+                     Fat = 3.5,
+                     Carbohydrates = 1.2,
+                     Protein = 26,
+                     Description = "A rich source of protein and nutrients, offering a variety of cuts and flavors to suit any dish."+ 
+                                    "Whether it's beef, pork, lamb, or other types, meat is a versatile ingredient that can be grilled, roasted, stewed, or fried."+ 
+                                    "It’s a staple in many cuisines, known for its hearty and satisfying taste.",
                      ImageUrl = "/images/meat.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 3,
                  },
                  new Product
                  {
-                     Name = "Chicken Breast",
-                     Energy = 2.0,
-                     Fat = 1.2,
-                     Carbohydrates = 3.2,
-                     Protein = 2.3,
-                     Description = "test",
+                     Name = "Chicken",
+                     Energy = 164,
+                     Fat = 3.6,
+                     Carbohydrates = 1,
+                     Protein = 31,
+                     Description = "A versatile and popular source of protein, enjoyed in cuisines around the world."+ 
+                                    "Its mild flavor and tender texture make it perfect for roasting, grilling, frying, or adding to soups and salads."+
+                                    "Chicken is a healthy choice, packed with essential nutrients and easy to prepare",
                      ImageUrl = "/images/chickenbreast.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 3,
@@ -232,11 +245,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Oatmilk",
-                     Energy = 3,
-                     Fat = 3,
-                     Carbohydrates = 2.3,
-                     Protein = 2,
-                     Description = "test",
+                     Energy = 46,
+                     Fat = 1.5,
+                     Carbohydrates = 6.7,
+                     Protein = 1,
+                     Description = "A creamy and plant-based alternative to dairy milk, made from oats and water."+ 
+                                    "It has a naturally sweet and mild flavor, making it perfect for coffee, smoothies, cereals, or baking."+ 
+                                    "Rich in fiber and often fortified with vitamins, oat milk is a nutritious and versatile choice for those seeking a dairy-free option.",
                      ImageUrl = "/images/oatmilk.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 2,
@@ -244,11 +259,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Apple",
-                     Energy = 3,
-                     Fat = 3,
-                     Carbohydrates = 2.3,
-                     Protein = 2,
-                     Description = "test",
+                     Energy = 73,
+                     Fat = 0.24,
+                     Carbohydrates = 19.3,
+                     Protein = 0.3,
+                     Description = "Apples are crisp and juicy fruits with a naturally sweet and refreshing flavor. Available in a variety of colors and tastes,"+ 
+                                    "from tart green to sweet red, they are perfect as a snack or for use in baking and cooking. Apples are rich in fiber and vitamins,"+ 
+                                    "making them a healthy choice for any time of the day.",
                      ImageUrl = "/images/apple.jpeg",
                      UserId = businessUser.Id,
                      CategoryId = 4,
@@ -256,11 +273,13 @@ public static class DBInit
                  new Product
                  {
                      Name = "Milk",
-                     Energy = 60,
-                     Fat = 6,
-                     Carbohydrates = 4.5,
-                     Protein = 8,
-                     Description = "this is milkkkkk",
+                     Energy = 42,
+                     Fat = 1,
+                     Carbohydrates = 5,
+                     Protein = 3.4,
+                     Description = "A nutritious and versatile drink, rich in calcium, protein, and essential vitamins."+ 
+                                    "It’s a staple in many diets, enjoyed on its own or used in cooking, baking, and beverages. Available in various options like whole,"+ 
+                                    "low-fat, or lactose-free, milk is perfect for all ages and lifestyles.",
                      ImageUrl = "/images/milk.jpg",
                      UserId = businessUser.Id,
                      CategoryId = 1,
